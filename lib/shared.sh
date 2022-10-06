fe8c60117e0979f1ff257f4c7696d5aaf04d66a6
gzliew <guozhangliew@gmail.com>
2022-10-06 13:10:56 +0800

    Remove echos

--- lib/shared.sh
+++ lib/shared.sh
@@ -4,10 +4,6 @@ CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 put_events()
 {
-  echo "--- [RNNNING COMMAND]"
-  echo "aws events put-events"
-  echo $1
-  echo $2
   aws events put-events "$1" "$2" --output json
 }
 
